5194edb95deb67e311bc6c698f46ff9a66f73343
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-03-16 15:57:35 -0300

    incluido a inicialização do mockito atraves de annotations, extraido para propriedade da classe as classes anotadas para realizacao de testes e simplificado o entendimento da classe

--- app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
+++ app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -28,7 +28,12 @@ import android.support.annotation.Nullable;
 import android.view.Display;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -43,21 +48,22 @@ import br.com.alura.leilao.model.Leilao;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class) //indicando a inicialização do mockitoannotations.initmocks....
 public class ListaLeilaoAdapterTest {
+
+    @Mock //passando a responsabilidade de injetar o objeto para o mockito
+    private Context context;
+
+    @Spy // passando a responsabilidade de injetar o objeto para o mockito
+    private ListaLeilaoAdapter listaLeilaoAdapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizarListaDeLeiloes_QuandoReceberListaDeLeiloes(){
-
-        //criado um objeto mock
-        //nao executa nada do fonte que estamos simulando
-        Context context = Mockito.mock(Context.class);
-
-        //cria um objeto espiao do mockito
-        // o spy executa o objeto da classe esperada no caso eu criei um context anteriormente.
-        ListaLeilaoAdapter listaLeilaoAdapter = Mockito.spy(new ListaLeilaoAdapter(context));
+//        MockitoAnnotations.initMocks(this); //metodo deprecated substituido pela abordagem da anotação de classe
         //no momento que estiver espiando o objeto nao vamos fazer nada quando chamar o metodo atualizaLista que encapsula notifydatasetchanged.
-        Mockito.doNothing().when(listaLeilaoAdapter).atualizaLista();
+        doNothing().when(listaLeilaoAdapter).atualizaLista();
 
         listaLeilaoAdapter.atualiza(new ArrayList<Leilao>(Arrays.asList(
                 new Leilao("Console"),
@@ -66,7 +72,7 @@ public class ListaLeilaoAdapterTest {
 
         int quantidadeDeLeiloes = listaLeilaoAdapter.getItemCount();
         //verifica o objeto mockado, e analisa se o metodo em questao foi chamado ou nao no caso o atualizalista
-        Mockito.verify(listaLeilaoAdapter).atualizaLista();
+        verify(listaLeilaoAdapter).atualizaLista();
         assertThat(quantidadeDeLeiloes, is(2));
 
     }

ba7845bf1fc09ebd5ea50d860dd0bd8a0ac59a39
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-03-17 09:26:36 -0300

    mockado a resposta da chamada do client, a requisicao da API foi simulada atraves da propriedade de resposta answer do mockito quando executado o client chamando o metodo todos

--- app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
+++ app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
@@ -4,11 +4,21 @@ import android.content.Context;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.api.retrofit.client.RespostaListener;
+import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -21,6 +31,8 @@ public class ListaLeilaoActivityTest {
     private Context context;
     @Spy
     private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
+    @Mock//por padrao objetos mockados nao fazem nada
+    private LeilaoWebClient client;
 
     @Test
     public void deve_Atualizar_ListaDeLeiloes_QuandoBuscarLeiLoesDaAPI() throws InterruptedException {
@@ -28,11 +40,28 @@ public class ListaLeilaoActivityTest {
 
         Mockito.doNothing().when(adapter).atualizaLista();
 
-        activity.buscaLeiloes(adapter);
-        Thread.sleep(3000);
+        //mockito executando uma resposta
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) { //mockado a respsota da requisicao
+                // o objeto e do mesmo tipo do argumetno requisitado dentro do client.
+                //caso tivesse mais de um argumento eu preciso indicar a posicao, no caso do client so possui um unico argumento do tipo do objeto declarado
+                RespostaListener<List<Leilao>> argument = invocation.getArgument(0);
+                argument.sucesso(new ArrayList<Leilao>(Arrays.asList(
+                        new Leilao("Computador"),
+                        new Leilao("Carro")
+                )));
+                return null;
+            }
+        }).when(client)//quando executado no client chamando o metodo todos
+                //passo atraves do argumentmatchers uma resposta qualquuer passando um listener qualquer...
+                .todos(ArgumentMatchers.any(RespostaListener.class));
+
+        activity.buscaLeiloes(adapter, client);
+
         int quantidadeDeLeiloesDevolvida = adapter.getItemCount();
 
-        assertThat(quantidadeDeLeiloesDevolvida, is(3));
+        assertThat(quantidadeDeLeiloesDevolvida, is(2));
     }
 
 }

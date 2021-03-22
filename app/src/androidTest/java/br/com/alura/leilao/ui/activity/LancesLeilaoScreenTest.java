c0ab99f38c20336989ba7e39377ef1d86be9fa9b
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-03-22 15:52:31 -0300

    Adicionado passo a passo para a realizacao de um large test

---
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoScreenTest.java
@@ -0,0 +1,31 @@
+package br.com.alura.leilao.ui.activity;
+
+import org.junit.Test;
+
+public class LancesLeilaoScreenTest {
+
+    @Test
+    public void deve_AtualizarLancesDoLeilao_QuandoReceberUmLance(){
+        //mapeamento de comportamentos que serao implementados
+        //salvar leilao na api
+        //inicializar a main activity
+        //clica no leilao cadastrado
+        //clica no floatactionbutton para adicionar novo lance
+        //verifica se aparece dialog com aviso que nao tem usuario e titulo para cadastrar usuario
+        //clica em cadastrar usuario dentro do dialog
+        //clica no FAB para cadastrar novo usuario - tela de lista de usuarios
+        //clica no edittext e preenche com o nome do usuario
+        //clica em adicionar
+        //clica no back do android e volta para a tela onde tem os lances do leilao
+        //clica no FAB lances do leilao
+        //verifica visibilidade do dialog com o titulo esperado
+        //clica no campo edittext do valor e preenche
+        //seleciona o usuario
+        //clica no botao propor para adicionar novo lance
+        //fazer assertion para as views de maior e menor lance
+        //fazer assertion para os maiores lances
+
+
+
+    }
+}

ff39ac48cf5770f1571138190bb6e1362bec1fe8
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-03-24 10:36:04 -0300

    refatorado o codigo e realizado algumas implementações a mais para garantir maior confiabilidade do teste

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoScreenTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoScreenTest.java
@@ -5,6 +5,7 @@ import android.support.test.espresso.Espresso;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
+import org.hamcrest.core.AllOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +24,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -71,52 +73,83 @@ public class LancesLeilaoScreenTest extends BaseTesteIntegracao {
 
         //salva leilao na api
         tentaSalvarLeilaoNaApi(new Leilao("Carro"));
+
         //inicializa activity
         activity.launchActivity(new Intent());
+
         //clica no leilao
         onView(withId(R.id.lista_leilao_recyclerview))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+
         //clica no fab da tela de lances do leilao
-        onView(withId(R.id.lances_leilao_fab_adiciona))
-                .perform(click());
+        onView(allOf(
+                withId(R.id.lances_leilao_fab_adiciona), isDisplayed()
+        )).perform(click());
+
         //verifica se aparece o dialog com o aviso por nao ter usuario com titulo e mensagem esperada
-        onView(withText("Usuários não encontrados"))
+        onView(allOf(withText("Usuários não encontrados"),
+                //encontrei o alertTitle alterando o texto acima provocando um erro, apos a exibicao da stack de erro eu busquei na hierarquia
+                //o res-name que seria o nome do recurso assim consigo realizar o match se o texto acima esta sendo apresentado no recurso esperado
+                withId(R.id.alertTitle)))
                 .check(matches(isDisplayed()));
-        onView(withText("Não existe usuários cadastrados! Cadastre um usuário para propor o lance."))
+
+        onView(allOf(
+                withText("Não existe usuários cadastrados! Cadastre um usuário para propor o lance."),
+                withId(android.R.id.message)))//tecnica utilizada descrita acima para buscar o nome do recurso neste caso referenciei o id do android e nao do meu pacote
                 .check(matches(isDisplayed()));
+
         //clica em cadastrar usuario dentro do dialog
-        onView(withText("Cadastrar usuário"))
+        onView(allOf(
+                withText("Cadastrar usuário"),
+                isDisplayed()))
                 .perform(click());
+
         //clica no FAB para cadastrar novo usuario - tela de lista de usuarios
         onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
                 isDisplayed()))
                 .perform(click());
+
         //clica no edittext e preenche com o nome do usuario
         onView(allOf(withId(R.id.form_usuario_input_text),
                 isDisplayed()))
-                .perform(replaceText("Joao"), closeSoftKeyboard());
+                .perform(
+                        click(),
+                        typeText("Joao"), //alterado para digitar o texto na view como o usuario
+                        closeSoftKeyboard());
+
         //clica em adicionar
         onView(allOf(
                 withId(android.R.id.button1), withText("Adicionar"),isDisplayed()
         )).perform(scrollTo(), click());
 
-
-
         //clica no back do android e volta para a tela onde tem os lances do leilao
         Espresso.pressBack();
 
         //clica no FAB
-        onView(withId(R.id.lances_leilao_fab_adiciona))
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),isDisplayed()))
                 .perform(click());
+
         //verifica visibilidade do dialog com o titulo esperado
-        onView(withText("Novo lance"))
+        onView(allOf(
+                withText("Novo lance"),
+                withId(R.id.alertTitle)))
                 .check(matches(isDisplayed()));
+
         //clica no edittext de valor e preenche
-        onView(withId(R.id.form_lance_valor_edittext))
-                .perform(click(),replaceText("200"), closeSoftKeyboard());
+        onView(allOf(
+                withId(R.id.form_lance_valor_edittext),
+                isDisplayed())
+        ).perform(
+                click(),
+                typeText("200"),
+                closeSoftKeyboard());
+
         //seleciona o usuario
-        onView(withId(R.id.form_lance_usuario))
-                .perform(click());
+        onView(allOf(
+                withId(R.id.form_lance_usuario),
+                isDisplayed())
+        ).perform(click());
+
         //indica que vamos pegar um usuario com um id e com o nome
         // o ondata faz a interação com a view e ele seleciona o objeto esperado
         onData(is(new Usuario(1,"Joao")))
@@ -125,7 +158,11 @@ public class LancesLeilaoScreenTest extends BaseTesteIntegracao {
                 .perform(click());
 
         //clica no botao propor para adicionar novo lance
-        onView(withText("Propor")).perform(click());
+        onView(allOf(
+                withText("Propor"),
+                isDisplayed())
+        ).perform(click());
+
         //fazer assertion para as views de maior e menor lance
         FormatadorDeMoeda formatador = new FormatadorDeMoeda();
         onView(withId(R.id.lances_leilao_maior_lance))
@@ -151,9 +188,5 @@ public class LancesLeilaoScreenTest extends BaseTesteIntegracao {
                         )
                 )
         );
-
-
-
-
     }
 }
